72a74c5149134a27c9e42e321fa72ad005e73837
Tonatiuh Banda <tonatiuhbanda@gmail.com>
2020-05-09 17:52:31 -0500

    Archivo para explorar los datos, archivo para predecir -Survived-

--- explore_data.py
+++ explore_data.py
@@ -1,76 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-from sklearn import preprocessing
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-
 
 #READ DATA
 file_train = 'titanic_kaggle/train.csv'
-data_train = pd.read_csv(file_train)
+data_raw = pd.read_csv(file_train)
 
 #EXPLORE
-#print(data_train.head())
-#print(data_train.count())
-
-'''
-plt.figure()
-plt.hist(data_train['Age'], bins=range(1, int(max(data_train['Age'])) + 1))
+print(data_raw.describe())
+print(data_raw.columns)
 
 plt.figure()
-plt.hist(data_train['Pclass'])
+plt.hist(data_raw['Age'], bins=range(1, int(max(data_raw['Age'])) + 1))
 
-'''
-#print(data_train.describe())
+plt.figure()
+plt.hist(data_raw['Pclass'])
 
-
-#TRANSFORM
-data_raw = data_train[['Survived', 'Age', 'Pclass', 'Sex']]
-data_raw = data_raw.dropna()
-enc = preprocessing.OrdinalEncoder()
-enc.fit(data_raw[['Sex']])
-transform_data = enc.transform(data_raw[['Sex']])
-data_raw['sex_enc'] = transform_data
-data_smart = data_raw[['Survived', 'Age', 'Pclass', 'sex_enc']]
-data_smart = data_smart.reset_index(drop=True)
-
-#CLASIFICATION
-train = data_smart.iloc[:600, :]
-test = data_smart.iloc[600:, :]
-
-clf_dt = tree.DecisionTreeClassifier()
-clf_dt = clf_dt.fit(train.iloc[:, 1:], train.iloc[:, 0])
-y_pred_dt = clf_dt.predict(test.iloc[:, 1:])
-
-clf_rf = RandomForestClassifier(n_estimators=20)
-clf_rf = clf_rf.fit(train.iloc[:, 1:], train.iloc[:, 0])
-y_pred_rf = clf_rf.predict(test.iloc[:, 1:])
-
-clf_gb = GradientBoostingClassifier()
-clf_gb = clf_gb.fit(train.iloc[:, 1:], train.iloc[:, 0])
-y_pred_gb = clf_gb.predict(test.iloc[:, 1:])
-
-#METRICS
-y_true = test.iloc[:, 0]
-print('DT')
-tn, fp, fn, tp = confusion_matrix(y_true, y_pred_dt).ravel()
-print(tn, fp, fn, tp)
-print(accuracy_score(y_true, y_pred_dt))
-print('')
-
-print('RF')
-tn, fp, fn, tp = confusion_matrix(y_true, y_pred_rf).ravel()
-print(tn, fp, fn, tp)
-print(accuracy_score(y_true, y_pred_rf))
-print('')
-
-print('GB')
-tn, fp, fn, tp = confusion_matrix(y_true, y_pred_gb).ravel()
-print(tn, fp, fn, tp)
-print(accuracy_score(y_true, y_pred_gb))
-print('')
+plt.figure()
+plt.hist(data_raw['Sex'])
---
+++ predict_survivors.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 16:42:55 2020
+
+@author: tonatiuh
+"""
+
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+#READ DATA
+file_train = 'titanic_kaggle/train.csv'
+data_raw = pd.read_csv(file_train)
+
+
+#TRANSFORM
+data_raw = data_raw[['Survived', 'Age', 'Pclass', 'Sex']]
+data_raw = data_raw.dropna()
+
+enc = preprocessing.OrdinalEncoder()
+enc.fit(data_raw[['Sex']])
+transform_data = enc.transform(data_raw[['Sex']])
+data_raw['sex_enc'] = transform_data
+data_smart = data_raw[['Survived', 'Age', 'Pclass', 'sex_enc']]
+data_smart = data_smart.reset_index(drop=True)
+
+
+#CLASIFICATION
+train = data_smart.iloc[:600, :]
+x_train = train.iloc[:, 1:]
+y_train = train.iloc[:, 0]
+
+test = data_smart.iloc[600:, :]
+x_test = test.iloc[:, 1:]
+y_test = test.iloc[:, 0]
+
+classifiers = {
+    'dt': {'model':tree.DecisionTreeClassifier()},
+    'rf': {'model':RandomForestClassifier(n_estimators=30)},
+    'gb': {'model':GradientBoostingClassifier()}
+    }
+
+for key in classifiers:
+    classifiers[key]['model'].fit(x_train, y_train)
+    classifiers[key]['y_pred'] = classifiers[key]['model'].predict(x_test)
+    
+    #METRICS
+    print(key)
+    tn, fp, fn, tp = confusion_matrix(y_test, classifiers[key]['y_pred']).ravel()
+    print(tn, fp, fn, tp)
+    print(accuracy_score(y_test, classifiers[key]['y_pred']))
+    print('')
